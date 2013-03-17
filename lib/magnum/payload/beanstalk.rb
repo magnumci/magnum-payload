e4500495fc818b6b564381d521eefa205312bf6a
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-03-17 00:07:26 -0500

    Test beanstalk subversion payload

--- lib/magnum/payload/beanstalk.rb
+++ lib/magnum/payload/beanstalk.rb
@@ -1,6 +1,16 @@
 module Magnum
   class Payload::Beanstalk < Payload::Base
     def parse!
+      data.revision ? parse_svn! : parse_git!
+    end
+
+    def last_commit
+      @last_commit ||= Hashr.new(data.commits.last)
+    end
+
+    private
+
+    def parse_git!
       @commit       = last_commit.id
       @branch       = data.branch
       @message      = last_commit.message
@@ -9,8 +19,15 @@ module Magnum
       @commit_url   = last_commit.url
     end
 
-    def last_commit
-      @last_commit ||= Hashr.new(data.commits.last)
+    def parse_svn!
+      @commit          = data.revision
+      @branch          = 'master'
+      @author          = data.author_full_name || data.author
+      @author_email    = data.author_email
+      @committer       = @author
+      @committer_email = @author_email
+      @commit_url      = data.changeset_url
+      @message         = data.message
     end
   end
 end

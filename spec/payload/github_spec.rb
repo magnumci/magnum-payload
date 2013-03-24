b368616863673fc8b9953e56281288d569b564ba
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-03-24 15:12:31 -0500

    Test new github branch payload

--- spec/payload/github_spec.rb
+++ spec/payload/github_spec.rb
@@ -6,46 +6,58 @@ describe Magnum::Payload::Github do
     let(:payload) { Magnum::Payload::Github.new(data) }
 
     it 'sets commit SHA' do
-      payload.commit.should eq('9d227f327e725164c3266be74cf5c00678edad13')
+      payload.commit.should eq '9d227f327e725164c3266be74cf5c00678edad13'
     end
 
     it 'sets commit branch' do
-      payload.branch.should eq('master')
+      payload.branch.should eq 'master'
     end
 
     it 'sets commit message' do
-      payload.message.should eq('Remove jruby from test matrix')
+      payload.message.should eq 'Remove jruby from test matrix'
     end
 
     it 'sets author and committer' do
-      payload.committer.should eq('Dan Sosedoff')
-      payload.committer_email.should eq('dan.sosedoff@gmail.com')
+      payload.committer.should eq 'Dan Sosedoff'
+      payload.committer_email.should eq 'dan.sosedoff@gmail.com'
 
-      payload.author.should eq('Dan Sosedoff')
-      payload.author_email.should eq('dan.sosedoff@gmail.com')
+      payload.author.should eq 'Dan Sosedoff'
+      payload.author_email.should eq 'dan.sosedoff@gmail.com'
     end
 
     it 'sets commit view url' do
-      payload.commit_url.should eq('https://github.com/sosedoff/lxc-ruby/commit/9d227f327e725164c3266be74cf5c00678edad13')
+      payload.commit_url.should eq 'https://github.com/sosedoff/lxc-ruby/commit/9d227f327e725164c3266be74cf5c00678edad13'
     end
 
     it 'sets compare url' do
-      payload.compare_url.should eq('https://github.com/sosedoff/lxc-ruby/compare/0e46f019e391...9d227f327e72')
+      payload.compare_url.should eq 'https://github.com/sosedoff/lxc-ruby/compare/0e46f019e391...9d227f327e72'
     end
 
-    context 'when payload is forced' do
-      let(:data) { fixture 'github/forced.json' }
+    context 'when push is forced' do
+      let(:data) { fixture('github/forced.json') }
 
       it 'parses head commit' do
         payload.commit.should eq 'e7508c4c70d2c5afc1e6c2f3a42ecc098f435103'
       end
     end
 
-    context 'when payload is deleted' do
-      let(:data) { fixture 'github/deleted.json' }
+    context 'for deleted branch' do
+      let(:data) { fixture('github/deleted.json') }
 
       it 'marks payload as skipped' do
         payload.skip.should be_true
+      end
+    end
+
+    context 'for new branch' do
+      let(:data) { fixture 'github/new_branch.json' }
+
+      it 'sets commit sha' do
+        payload.commit.should eq 'd9cc46f9e7e6aa65df696d8d1efe86de755b46ae'
+      end
+
+      it 'sets branch' do
+        payload.branch.should eq 'hello'
       end
     end
   end

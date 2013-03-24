62f2823696d633cfed4bfa706ba7d2baf01dc507
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-03-23 21:13:36 -0500

    Add gitlab integration

--- lib/magnum/payload/gitlab.rb
+++ lib/magnum/payload/gitlab.rb
@@ -1,4 +1,18 @@
 module Magnum
   class Payload::Gitlab < Payload::Base
+    def parse!
+      @commit          = data.after
+      @branch          = data.ref.split('/').last
+      @author          = last_commit.author.name
+      @author_email    = last_commit.author.email
+      @committer       = @author
+      @committer_email = @author_email
+      @message         = last_commit.message
+      @commit_url      = last_commit.url
+    end
+
+    def last_commit
+      @last_commit ||= Hashr.new(data.commits.last)
+    end
   end
 end
--- spec/payload/gitlab_spec.rb
+++ spec/payload/gitlab_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Magnum::Payload::Gitlab do
-  pending 'Needs tests'
+  let(:data)    { fixture('gitlab/commits.json') }
+  let(:payload) { Magnum::Payload::Gitlab.new(data) }
+
+  describe '#parse!' do
+    it 'sets commit SHA' do
+      payload.commit.should eq 'a96a0df33262e2d2fd1b20162553aae6750f8c00'
+    end
+
+    it 'sets commit branch' do
+      payload.branch.should eq 'master'
+    end
+
+    it 'sets commit message' do
+      payload.message.should eq 'Commit 2'
+    end
+
+    it 'sets author and committer' do
+      payload.committer.should eq 'Dan Sosedoff'
+      payload.author.should eq 'Dan Sosedoff'
+    end
+
+    it 'sets commit view url' do
+      payload.commit_url.should eq 'https://gitlab.com/sosedoff/bar/commit/a96a0df33262e2d2fd1b20162553aae6750f8c00'
+    end
+
+    it 'does not set compare url' do
+      payload.compare_url.should eq nil
+    end
+  end
 end

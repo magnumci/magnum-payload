d7e5268de142060415cd8a6d90df8203f3a18a47
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-27 22:55:38 -0500

    Keep parser structure consistent

--- lib/magnum/payload/custom.rb
+++ lib/magnum/payload/custom.rb
@@ -1,6 +1,12 @@
 module Magnum
   class Payload::Custom < Payload::Base
     def parse!
+      assign_payload
+    end
+
+    private
+
+    def assign_payload
       @commit          = data.commit
       @author          = data.author
       @committer       = data.committer
--- lib/magnum/payload/gitslice.rb
+++ lib/magnum/payload/gitslice.rb
@@ -1,6 +1,12 @@
 module Magnum
   class Payload::Gitslice < Payload::Base
     def parse!
+      assign_payload
+    end
+
+    private
+
+    def assign_payload
       @commit          = data.commit
       @author          = data.author
       @author_email    = data.author_email

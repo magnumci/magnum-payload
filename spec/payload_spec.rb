94f07e9987a0f7939cef94e88c666dac267a069b
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-07-20 14:20:33 -0500

    Refactor payload class specs to use new matchers

--- spec/payload_spec.rb
+++ spec/payload_spec.rb
@@ -4,27 +4,27 @@ describe Magnum::Payload do
   describe '.parse' do
     it 'returns payload instance for github' do
       payload = Magnum::Payload.parse('github', fixture('github.json'))
-      payload.should be_a Magnum::Payload::Github
+      expect(payload).to be_a Magnum::Payload::Github
     end
 
     it 'returns payload instance for gitslice' do
       payload = Magnum::Payload.parse('gitslice', fixture('gitslice.json'))
-      payload.should be_a Magnum::Payload::Gitslice
+      expect(payload).to be_a Magnum::Payload::Gitslice
     end
 
     it 'returns payload instance for gitlab' do
       payload = Magnum::Payload.parse('gitlab', fixture('gitlab/commits.json'))
-      payload.should be_a Magnum::Payload::Gitlab
+      expect(payload).to be_a Magnum::Payload::Gitlab
     end
 
     it 'returns payload instance for bitbucket' do
       payload = Magnum::Payload.parse('bitbucket', fixture('bitbucket/git.json'))
-      payload.should be_a Magnum::Payload::Bitbucket
+      expect(payload).to be_a Magnum::Payload::Bitbucket
     end
 
     it 'returns payload instance for beanstalk' do
       payload = Magnum::Payload.parse('beanstalk', fixture('beanstalk/git.json'))
-      payload.should be_a Magnum::Payload::Beanstalk
+      expect(payload).to be_a Magnum::Payload::Beanstalk
     end
 
     it 'raises error if source is invalid' do

9832ca8dde771e37b0a1d0e264cbc9b4f576c2e9
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-09-20 18:39:35 -0500

    Replace single quotes with double quotes

--- spec/payload/gitlab_spec.rb
+++ spec/payload/gitlab_spec.rb
@@ -1,51 +1,51 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Magnum::Payload::Gitlab do
-  let(:data)    { fixture('gitlab/commits.json') }
+  let(:data)    { fixture("gitlab/commits.json") }
   let(:payload) { Magnum::Payload::Gitlab.new(data) }
 
-  describe '#parse!' do
-    it 'sets commit SHA' do
-      payload.commit.should eq 'a96a0df33262e2d2fd1b20162553aae6750f8c00'
+  describe "#parse!" do
+    it "sets commit SHA" do
+      payload.commit.should eq "a96a0df33262e2d2fd1b20162553aae6750f8c00"
     end
 
-    it 'sets commit branch' do
-      payload.branch.should eq 'master'
+    it "sets commit branch" do
+      payload.branch.should eq "master"
     end
 
-    it 'sets commit message' do
-      payload.message.should eq 'Commit 2'
+    it "sets commit message" do
+      payload.message.should eq "Commit 2"
     end
 
-    it 'sets author and committer' do
-      payload.committer.should eq 'Dan Sosedoff'
-      payload.author.should eq 'Dan Sosedoff'
+    it "sets author and committer" do
+      payload.committer.should eq "Dan Sosedoff"
+      payload.author.should eq "Dan Sosedoff"
     end
 
-    it 'sets commit view url' do
-      payload.commit_url.should eq 'https://gitlab.com/sosedoff/bar/commit/a96a0df33262e2d2fd1b20162553aae6750f8c00'
+    it "sets commit view url" do
+      payload.commit_url.should eq "https://gitlab.com/sosedoff/bar/commit/a96a0df33262e2d2fd1b20162553aae6750f8c00"
     end
 
-    it 'does not set compare url' do
+    it "does not set compare url" do
       payload.compare_url.should eq nil
     end
 
-    context 'on new branch' do
-      let(:data) { fixture('gitlab/create_branch.json') }
+    context "on new branch" do
+      let(:data) { fixture("gitlab/create_branch.json") }
 
-      it 'sets commit SHA' do
-        payload.commit.should eq '0188ef243dd8083a4d4761766342b523d521247d'
+      it "sets commit SHA" do
+        payload.commit.should eq "0188ef243dd8083a4d4761766342b523d521247d"
       end
 
-      it 'sets branch to foobar' do
-        payload.branch.should eq 'foobar'
+      it "sets branch to foobar" do
+        payload.branch.should eq "foobar"
       end
     end
 
-    context 'on deleted branch' do
-      let(:data) { fixture('gitlab/delete_branch.json') }
+    context "on deleted branch" do
+      let(:data) { fixture("gitlab/delete_branch.json") }
 
-      it 'sets skip to true ' do
+      it "sets skip to true " do
         payload.skip.should eq true
       end
     end

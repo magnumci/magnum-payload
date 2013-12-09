c292a217f6c4048a9bee864549e0cfa733539d1a
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-12-09 00:56:27 -0600

    Refactor message parser spec

--- spec/payload/message_parser_spec.rb
+++ spec/payload/message_parser_spec.rb
@@ -1,26 +1,29 @@
 require "spec_helper"
 
+class TestMessageParser
+  include Magnum::Payload::MessageParser
+end
+
 describe Magnum::Payload::MessageParser do
-  let(:klass) { class Klass ; include Magnum::Payload::MessageParser ; end }
-  let(:subject) { klass.new }
+  let(:subject) { TestMessageParser.new }
 
   describe "#skip_message?" do
-    it "returns true when message contains "ci-skip"" do
+    it "returns true when message contains 'ci-skip'" do
       subject.stub(:message).and_return("Commit message [ci-skip]")
       subject.skip_message?.should eq true
     end
 
-    it "returns true when message contains "ci skip"" do
+    it "returns true when message contains 'ci skip'" do
       subject.stub(:message).and_return("Commit message [ci skip]")
       subject.skip_message?.should eq true
     end
 
-    it "returns true when message contains "skip ci"" do
+    it "returns true when message contains 'skip ci'" do
       subject.stub(:message).and_return("Commit message [skip ci]")
       subject.skip_message?.should eq true
     end
 
-    it "returns true when message contains "skip-ci"" do
+    it "returns true when message contains 'skip-ci'" do
       subject.stub(:message).and_return("Commit message [skip-ci]")
       subject.skip_message?.should eq true
     end

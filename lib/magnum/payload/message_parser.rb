d10050ab106cd4e5c4351bfff937b159bcf1a1ae
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-12-09 01:14:59 -0600

    Compact message parser namespace

--- lib/magnum/payload/message_parser.rb
+++ lib/magnum/payload/message_parser.rb
@@ -1,11 +1,9 @@
 module Magnum
-  module Payload
-    module MessageParser
-      REGEX_SKIP = /\[(ci skip|ci-skip|skip ci|skip-ci)\]/i
+  module Payload::MessageParser
+    REGEX_SKIP = /\[(ci skip|ci-skip|skip ci|skip-ci)\]/i
 
-      def skip_message?
-        message.to_s.split("\n").first =~ REGEX_SKIP ? true : false
-      end
+    def skip_message?
+      message.to_s.split("\n").first =~ REGEX_SKIP ? true : false
     end
   end
 end

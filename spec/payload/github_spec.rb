1d9c084885082843eeb34b11e8ce0ebb0ac7d30a
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-02-14 19:15:32 -0600

    Convert github tests to a newer rspec syntax

--- spec/payload/github_spec.rb
+++ spec/payload/github_spec.rb
@@ -6,40 +6,40 @@ describe Magnum::Payload::Github do
     let(:payload) { Magnum::Payload::Github.new(data) }
 
     it "sets commit sha" do
-      payload.commit.should eq "9d227f327e725164c3266be74cf5c00678edad13"
+      expect(payload.commit).to eq "9d227f327e725164c3266be74cf5c00678edad13"
     end
 
     it "sets commit branch" do
-      payload.branch.should eq "master"
+      expect(payload.branch).to eq "master"
     end
 
     it "sets commit message" do
-      payload.message.should eq "Remove jruby from test matrix"
+      expect(payload.message).to eq "Remove jruby from test matrix"
     end
 
     it "sets author" do
-      payload.committer.should eq "Dan Sosedoff"
-      payload.committer_email.should eq "dan.sosedoff@gmail.com"
+      expect(payload.committer).to eq "Dan Sosedoff"
+      expect(payload.committer_email).to eq "dan.sosedoff@gmail.com"
     end
 
     it "sets committer" do
-      payload.author.should eq "Dan Sosedoff"
-      payload.author_email.should eq "dan.sosedoff@gmail.com"
+      expect(payload.author).to eq "Dan Sosedoff"
+      expect(payload.author_email).to eq "dan.sosedoff@gmail.com"
     end
 
     it "sets commit view url" do
-      payload.commit_url.should eq "https://github.com/sosedoff/lxc-ruby/commit/9d227f327e725164c3266be74cf5c00678edad13"
+      expect(payload.commit_url).to eq "https://github.com/sosedoff/lxc-ruby/commit/9d227f327e725164c3266be74cf5c00678edad13"
     end
 
     it "sets commit compare url" do
-      payload.compare_url.should eq "https://github.com/sosedoff/lxc-ruby/compare/0e46f019e391...9d227f327e72"
+      expect(payload.compare_url).to eq "https://github.com/sosedoff/lxc-ruby/compare/0e46f019e391...9d227f327e72"
     end
 
     context "when push is forced" do
       let(:data) { fixture "github/forced.json" }
 
       it "parses head commit" do
-        payload.commit.should eq "e7508c4c70d2c5afc1e6c2f3a42ecc098f435103"
+        expect(payload.commit).to eq "e7508c4c70d2c5afc1e6c2f3a42ecc098f435103"
       end
     end
 
@@ -47,7 +47,7 @@ describe Magnum::Payload::Github do
       let(:data) { fixture "github/deleted.json" }
 
       it "marks payload as skipped" do
-        payload.skip.should be_true
+        expect(payload.skip).to be_true
       end
     end
 
@@ -55,11 +55,11 @@ describe Magnum::Payload::Github do
       let(:data) { fixture "github/new_branch.json" }
 
       it "sets commit sha" do
-        payload.commit.should eq "d9cc46f9e7e6aa65df696d8d1efe86de755b46ae"
+        expect(payload.commit).to eq "d9cc46f9e7e6aa65df696d8d1efe86de755b46ae"
       end
 
       it "sets branch" do
-        payload.branch.should eq "hello"
+        expect(payload.branch).to eq "hello"
       end
     end
 
@@ -67,7 +67,7 @@ describe Magnum::Payload::Github do
       let(:data) { fixture "github/new_tag.json" }
 
       it "marks payload as skipped" do
-        payload.skip.should be_true
+        expect(payload.skip).to be_true
       end
     end
 

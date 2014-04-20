75ac4f6446f4b0cba30bc77bdede1565fd4c0157
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-04-20 11:05:28 -0500

    Refactor base spec

--- spec/payload/base_spec.rb
+++ spec/payload/base_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Magnum::Payload::Base do
   let(:base) { described_class }
@@ -7,48 +7,50 @@ describe Magnum::Payload::Base do
     Magnum::Payload::Base.any_instance.stub(:parse!)
   end
 
-  describe '#new' do
-    it 'assigns raw data attribute' do
+  describe "#new" do
+    it "assigns raw data attribute" do
       Magnum::Payload::Base.any_instance.stub(:parse_payload)
 
       payload = base.new("raw data")
-      payload.raw_data.should eq 'raw data'
+      expect(payload.raw_data).to eq "raw data"
     end
 
-    it 'assigns data attribute' do
-      payload = base.new({'foo' => 'bar'})
-      payload.data.should be_a Hashr
+    it "assigns data attribute" do
+      payload = base.new({"foo" => "bar"})
+      expect(payload.data).to be_a Hashr
     end
 
-    it 'accepts a String' do
+    it "accepts a string" do
       payload = base.new('{"foo":"bar"}')
-      payload.data.should be_a Hashr
-      payload.data.foo.should eq 'bar'
+
+      expect(payload.data).to be_a Hashr
+      expect(payload.data.foo).to eq "bar"
     end
 
-    it 'accepts a Hash' do
-      payload = base.new({'foo' => 'bar'})
-      payload.data.should be_a Hashr
-      payload.data.foo.should eq('bar')
+    it "accepts a hash" do
+      payload = base.new({"foo" => "bar"})
+
+      expect(payload.data).to be_a Hashr
+      expect(payload.data.foo).to  eq "bar"
     end
 
-    it 'raises error on invalid input type' do
+    it "raises error on invalid input type" do
       expect { base.new(nil) }.to raise_error "String or Hash required"
     end
 
-    it 'raises error on invalid json' do
+    it "raises error on invalid json" do
       expect { base.new("invalid json") }.
         to raise_error Magnum::Payload::ParseError, "Valid JSON required"
     end
   end
 
-  describe '#skip' do
+  describe "#skip" do
     before do
       Magnum::Payload::Base.any_instance.stub(:parse_payload)
     end
 
-    it 'should be false' do
-      base.new('data').skip.should eq false
+    it "returns false" do
+      expect(base.new("data").skip).to be_false
     end
   end
 

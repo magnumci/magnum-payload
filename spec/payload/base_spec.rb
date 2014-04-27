b089fd462b6c45f9e7528222faca5f4987a9e1b9
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-04-26 21:44:45 -0500

    Add tests to base.parse

--- spec/payload/base_spec.rb
+++ spec/payload/base_spec.rb
@@ -3,11 +3,11 @@ require "spec_helper"
 describe Magnum::Payload::Base do
   let(:base) { described_class }
 
-  before do
-    Magnum::Payload::Base.any_instance.stub(:parse!)
-  end
-
   describe "#new" do
+    before do
+      Magnum::Payload::Base.any_instance.stub(:parse!)
+    end
+
     it "assigns raw data attribute" do
       Magnum::Payload::Base.any_instance.stub(:parse_payload)
 
@@ -46,6 +46,7 @@ describe Magnum::Payload::Base do
 
   describe "#skip" do
     before do
+      Magnum::Payload::Base.any_instance.stub(:parse!)
       Magnum::Payload::Base.any_instance.stub(:parse_payload)
     end
 
@@ -56,12 +57,20 @@ describe Magnum::Payload::Base do
 
   describe "#skip?" do
     before do
+      Magnum::Payload::Base.any_instance.stub(:parse!)
       Magnum::Payload::Base.any_instance.stub(:parse_payload)
       Magnum::Payload::Base.any_instance.stub(:skip) { true }
     end
 
     it "returns true if skip" do
       expect(base.new("data").skip?).to be_true
+    end
+  end
+
+  describe "#parse!" do
+    it "raises error" do
+      expect { described_class.new("foo" => "bar").parse! }.
+        to raise_error "Not implemented in base class"
     end
   end
 end

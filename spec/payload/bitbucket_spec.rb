3b7fc62e1f4c0d925a3623103440b5551e3cb7de
Dan Sosedoff <dan.sosedoff@gmail.com>
2015-06-18 18:44:43 -0500

    Shorten bitbucket tests

--- spec/payload/bitbucket_spec.rb
+++ spec/payload/bitbucket_spec.rb
@@ -8,46 +8,22 @@ describe Magnum::Payload::Bitbucket do
     context "with git payload" do
       let(:data) { fixture("bitbucket/git.json") }
 
-      it "sets commit SHA" do
+      it "parses payload" do
         expect(payload.commit).to eq "f15566c42759198fd32a70963d2509f3f8309586"
-      end
-
-      it "sets commit branch" do
         expect(payload.branch).to eq "master"
-      end
-
-      it "sets commit message" do
         expect(payload.message).to eq "Commit Sat Jan 19 18:42:40 CST 2013"
-      end
-
-      it "sets committer name" do
         expect(payload.committer).to eq nil
-      end
-
-      it "sets committer email" do
         expect(payload.committer_email).to eq nil
-      end
-
-      it "sets author name" do
         expect(payload.author).to eq "Dan Sosedoff"
-      end
-
-      it "sets author email" do
         expect(payload.author_email).to eq "dan.sosedoff@gmail.com"
-      end
-
-      it "sets commit view url" do
         expect(payload.commit_url).to eq "https://bitbucket.org/sosedoff/test1/commits/f15566c42759198fd32a70963d2509f3f8309586"
-      end
-
-      it "sets compare url" do
         expect(payload.compare_url).to eq "https://bitbucket.org/sosedoff/test1/compare/e15c6013c0f6232153e53b003b97da51d338da3a..f15566c42759198fd32a70963d2509f3f8309586"
       end
 
-      context "that does not have commits" do
+      context "when no commits" do
         let(:data) { fixture("bitbucket/git_no_commits.json") }
 
-        it "raises PayloadError exception" do
+        it "raises an exception" do
           expect { described_class.new(data) }.
             to raise_error Magnum::Payload::PayloadError, "Payload has no commits"
         end
@@ -57,31 +33,13 @@ describe Magnum::Payload::Bitbucket do
     context "with mercurial payload" do
       let(:data) { fixture("bitbucket/hg.json") }
 
-      it "sets commit SHA" do
+      it "parses payload" do
         expect(payload.commit).to eq 4
-      end
-
-      it "sets commit branch" do
         expect(payload.branch).to eq "default"
-      end
-
-      it "sets commit message" do
         expect(payload.message).to eq "Commit 2"
-      end
-
-      it "sets author name" do
-        payload.author.should eq "Dan Sosedoff"
-      end
-
-      it "sets author email" do
+        expect(payload.author).to eq "Dan Sosedoff"
         expect(payload.author_email).to eq "dan.sosedoff@gmail.com"
-      end
-
-      it "sets commit view url" do
         expect(payload.commit_url).to eq "https://bitbucket.org/sosedoff/mercurial-test/commits/4"
-      end
-
-      it "sets compare url" do
         expect(payload.compare_url).to eq "https://bitbucket.org/sosedoff/mercurial-test/compare/3..4"
       end
     end
